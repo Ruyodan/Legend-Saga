debdd4c639ad01573405e1e8f90359effc15c407
Ruyodan <salvator.hamdoune@gmail.com>
2024-09-10 14:50:56 +0000

    Add routing to whatever name in the path

--- backend/app.py
+++ backend/app.py
@@ -7,5 +7,11 @@ def index():
         return 'Hello world'
 
 
+
+@app.route('/<name>')  
+def printname(name):
+            return f'Welcome {name}'
+
+
 if __name__ == '__main__':
         app.run(debug=True)
