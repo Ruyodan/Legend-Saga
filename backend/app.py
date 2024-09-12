11ac70ac7078d943cf977d6d3aa2c2f55200dddc
Ruyodan <salvator.hamdoune@gmail.com>
2024-09-12 18:57:14 +0000

    Return AI category questions

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,30 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
+questions_path_AI = './questions/AI.json'
 
+#the root of the app what the user gets when he accesses our api without specifying a path
 @app.route('/')
 def index():
         return 'Hello world'
 
 
+#what the user gets when he accesses 127.0.0.1:5000/AI in the path 
+@app.route('/AI')
+def print():
+        with open(questions_path_AI, 'r') as file:
+                data = json.load(file)
+        return data
 
+#what the user gets when he accesses ater puting any names after /
 @app.route('/<name>')  
 def printname(name):
             return f'Welcome {name}'
+       
+
+
+
 
 
 if __name__ == '__main__':
