62a387caefdaddb60bf7fbf4103e15625c3d2673
Irbain <contact@irbaine.com>
2024-09-18 18:55:21 +0100

    Done

---
+++ app/api/proxy.ts
@@ -0,0 +1,8 @@
+// import type { NextApiRequest, NextApiResponse } from 'next';
+
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   const apiUrl = 'http://ad8b4b2c04ec.115532cb.alx-cod.online:5000/go/ai';
+//   const response = await fetch(apiUrl);
+//   const data = await response.json();
+//   res.status(200).json(data);
+// }
